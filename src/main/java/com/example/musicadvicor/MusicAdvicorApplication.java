73dd79ee82169e5059a4a68850d9867ba285e177
JurekMateusz <mateuszjurek98@outlook.com>
2020-11-06 10:04:22 +0100

    Spring-boot init

---
+++ src/main/java/com/example/musicadvicor/MusicAdvicorApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicadvicor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAdvicorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicAdvicorApplication.class, args);
+    }
+
+}

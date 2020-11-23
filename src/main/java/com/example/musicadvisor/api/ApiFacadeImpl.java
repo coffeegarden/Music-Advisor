d4952540d8d3a0aaece3108ab0281ceb746c0beb
JurekMateusz <mateuszjurek98@outlook.com>
2020-11-23 21:46:18 +0100

    Refactor com.example.musicadvisor.api package

--- src/main/java/com/example/musicadvisor/api/ApiFacadeImpl.java
+++ src/main/java/com/example/musicadvisor/api/ApiFacadeImpl.java
@@ -2,32 +2,31 @@ package com.example.musicadvisor.api;
 
 import com.example.musicadvisor.api.model.album.Album;
 import com.example.musicadvisor.api.model.albums.Albums;
+import com.example.musicadvisor.api.model.albums.AlbumsRoot;
+import com.example.musicadvisor.api.url.ApiUrlBuilder;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
+@AllArgsConstructor
 public class ApiFacadeImpl implements ApiFacade {
     private RequestApi request;
     private ApiUrlBuilder urlBuilder;
 
-    public ApiFacadeImpl(RequestApi request, ApiUrlBuilder urlBuilder) {
-        this.request = request;
-        this.urlBuilder = urlBuilder;
-    }
-
     @Override
     public Albums getNewReleases(int page) {
         String url = urlBuilder.newReleases(page - 1);
-        Optional<Albums> newReleases = request.getNewReleases(url);
-        return newReleases.orElse(Albums.empty());
+        Optional<AlbumsRoot> root = (Optional<AlbumsRoot>) request.getCollection(AlbumsRoot.class, url);
+        AlbumsRoot albumsRoot = root.orElseThrow(IllegalStateException::new);
+        return albumsRoot.getAlbums();
     }
 
     @Override
-    public Optional<Album> getAlbum( String id) {
+    public Optional<Album> getAlbum(String id) {
         String url = urlBuilder.album(id);
-        Optional<Album> album = request.getAlbum(url);
-        return album;
+        Optional<Album> collection = (Optional<Album>) request.getCollection(Album.class, url);
+        return collection;
     }
-
 }

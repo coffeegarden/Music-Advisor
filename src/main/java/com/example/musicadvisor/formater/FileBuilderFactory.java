27b60b18ee23ee031d493b5d70b1fc7e634af825
JurekMateusz <mateuszjurek98@outlook.com>
2020-11-29 12:03:03 +0100

    Modify Factory Method, return everytime new instance of parser.

--- src/main/java/com/example/musicadvisor/formater/FileBuilderFactory.java
+++ src/main/java/com/example/musicadvisor/formater/FileBuilderFactory.java
@@ -5,20 +5,46 @@ import com.example.musicadvisor.formater.filebuilder.format.json.JsonFileBuilder
 import com.example.musicadvisor.formater.filebuilder.format.xml.XmlFileBuilder;
 import com.google.common.collect.ImmutableMap;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
 import java.util.Optional;
 
 public class FileBuilderFactory {
-    private static ImmutableMap<String, FileBuilder> map;
+    private static ImmutableMap<String, String> map;
 
     static {
-        map = ImmutableMap.<String, FileBuilder>builder()
-                .put("JSON", new JsonFileBuilder())
-                .put("XML", new XmlFileBuilder())
+        map = ImmutableMap.<String, String>builder()
+                .put("JSON", JsonFileBuilder.class.getName())
+                .put("XML", XmlFileBuilder.class.getName())
                 .build();
     }
 
-    public static Optional<FileBuilder> get(String builder) {
-        String formatted = builder.trim().toUpperCase();
-        return Optional.ofNullable(map.get(formatted));
+    public static Optional<FileBuilder> get(String type) {
+        String classToCreate = getFromMap(type);
+        if (Objects.isNull(classToCreate)) return Optional.empty();
+        FileBuilder instance = getInstance(classToCreate);
+        return Optional.ofNullable(instance);
+    }
+
+    private static String getFromMap(String type) {
+        String formatted = type.trim().toUpperCase();
+        String classToCreate = map.get(formatted);
+        return classToCreate;
+    }
+
+    private static FileBuilder getInstance(String classToCreate) {
+        try {
+            Constructor<?> ctor = getConstructor(classToCreate);
+            return (FileBuilder) ctor.newInstance();
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static Constructor<?> getConstructor(String classToCreate) throws ClassNotFoundException, NoSuchMethodException {
+        Class<?> clazz = Class.forName(classToCreate);
+        return clazz.getConstructor();
     }
 }
